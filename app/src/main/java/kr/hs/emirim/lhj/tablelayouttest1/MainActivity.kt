98ff7d3125311471cdb8ea4ce986878dc53a41c6
dinmoy <imhyojin614@gmail.com>
2023-06-13 19:53:01 +0900

    TableLayout 사용한 계산기(키패드 있음)

--- app/src/main/java/kr/hs/emirim/lhj/tablelayouttest1/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/lhj/tablelayouttest1/MainActivity.kt
@@ -2,8 +2,11 @@ package kr.hs.emirim.lhj.tablelayouttest1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.OnClickListener
+import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit internal var edit1: EditText
@@ -23,12 +26,49 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ex5_2)
-        edit1=findViewById(R.id.edit1)
-        edit2=findViewById(R.id.edit2)
-        btnPlus=findViewById(R.id.btn_plus)
-        btnMinus=findViewById(R.id.btn_minus)
-        btnMultiply=findViewById(R.id.btn_multiple)
-        btnDivide=findViewById(R.id.btn_divide)
-        textResult=findViewById(R.id.testresult)
+        edit1 = findViewById(R.id.edit1)
+        edit2 = findViewById(R.id.edit2)
+        btnPlus = findViewById(R.id.btn_plus)
+        btnMinus = findViewById(R.id.btn_minus)
+        btnMultiply = findViewById(R.id.btn_multiple)
+        btnDivide = findViewById(R.id.btn_divide)
+        textResult = findViewById(R.id.testresult)
+
+        btnPlus.setOnClickListener(btnListener)
+        btnMinus.setOnClickListener(btnListener)
+        btnMultiply.setOnClickListener(btnListener)
+        btnDivide.setOnClickListener(btnListener)
+
+        for(i in 0..btnIds.size-1 step 1){
+            btns.add(findViewById(btnIds[i]))
+        }
+        for(i in 0..btnIds.size-1 step 1){
+            btns[i].setOnClickListener{
+                if(edit1.isFocused){
+                    num1=edit1.text.toString()+btns[i].text.toString()
+                    edit1.setText(num1)
+                }else if(edit2.isFocused){
+                    num2=edit2.text.toString()+btns[i].text.toString()
+                    edit2.setText(num2)
+                }else{
+                    Toast.makeText(applicationContext,"먼저 입력할 곳에 포커스를 주세요",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
+    var btnListener = OnClickListener {
+        num1 = edit1.text.toString()
+        num2 = edit2.text.toString()
+
+
+        when (it.id) {
+            R.id.btn_plus -> result = num1.toInt() + num2.toInt()
+            R.id.btn_minus -> result = num1.toInt() - num2.toInt()
+            R.id.btn_multiple -> result = num1.toInt() * num2.toInt()
+            R.id.btn_divide -> result = num1.toInt() / num2.toInt()
+        }
+        textResult.setText(R.string.text_result)
+        textResult.append(result.toString())
+    }
+
 }
